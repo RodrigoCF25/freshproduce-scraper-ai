b7381d0c82ad40ebf55fd0577a85b2813b4af979
RodrigoCF25 <rodrigocf88@hotmail.com>
2025-07-10 19:48:55 -0600

    removed prints and added a comment to recall user to create a .env file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fileutils.csvHandler import CSVHandler
 import asyncio
 from ai.gemini import Gemini
 
+#Remember to have a .env file with the env variable GEMINI_API_KEY="{your api key}"
 
 if __name__ == "__main__":
     scrapper = FreshProduceArticlesScraper(["Global Trade","Food Safety","Technology"])
@@ -11,10 +12,6 @@ if __name__ == "__main__":
         scraped_filepath = "scraped_data.csv"
         articles = await scrapper.scrape()
         articles_as_dict = (article.core_info() for article in articles)
-        for n,article in enumerate(articles,1):
-            print(n)
-            print(article.title)
-
         CSVHandler.write(scraped_filepath, articles_as_dict)
         
         #Second Stage: AI must get a summary and a topics list for each summary. Save articles with these insights in a CSV
